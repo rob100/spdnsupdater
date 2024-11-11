33a72b882b7f7da2b8416403936565759d149045
Robin Schlegel <rob100@web.de>
2024-11-11 13:57:24 +0100

    Ipv6 (#1)
    * Added IPv6 support
    
    * added alternative way to get ipv
    
    * Fixed syntax error and improved logging
    
    ---------
    
    Co-authored-by: Robin Schlegel <rschlegel@thinkport.digital>

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 ### What is it for?
-This bash script is to be used for the Securepoint Dynamic DNS Service (https://www.spdns.de/).
+This bash script is to be used for the Securepoint Dynamic DNS Service (https://www.spdns.de/, https://www.spdyn.de/).
 
 ### How to use?
-Add your domain and password under `~/.spdnsupdater.conf`
+Add your domain and password/updateToken under `~/.spdnsupdater.conf`
 Example:
 ```
 DOMAIN=(dmxxxx.spdns.eu dyxxxx.spdns.eu qxxxx.spdns.eu upxxxx.spdns.eu)
@@ -11,10 +11,13 @@ PASSWORD=(ieya-xxxxx qsto-xxxxxx mnuq-xxxxx zfoy-xxxxx)
 Run the script `bash spdnsupdater.sh`
 
 ### How it works?
-1.	Load “lastip” and “lastcheck” from /tmp/lastip (if available)
+1.	Load “lastip” from /tmp/lastip (if available)
 2.	Get the current ip adress from “http://checkip4.spdyn.de/”
 3.	Check if the ip was changed (since the last run)
 4.	Checks when the last update was done (atm after 24h the script force the ip update).
-5.	Save “lastip” and “lastcheck” to /tmp/lastip
+5.	Save “lastip”  to /tmp/lastip
 6.	Load conf file
 7.	Perform the ip update (if the ip was changed or the last check is older as 24h
+
+### Update 01/25/24
+- Added IPv6 support (If domain name is compatible both IPv4 and IPv6 will be set)
--- spdnsupdater.sh
+++ spdnsupdater.sh
@@ -2,113 +2,116 @@
 
 # load last IP
 LASTIP=0.0.0.0
+LASTIP6=0:0:0:0:0:0:0:0
 
-if [ -f /tmp/lastip ];then
+if [ -f /tmp/lastip ]; then
 	echo "import lastip"
 	source /tmp/lastip
 	echo "last ip was $LASTIP"
 fi
 
-
+if [ -f /tmp/lastip6 ]; then
+	echo "import lastip6"
+	source /tmp/lastip6
+	echo "last ipv6 was $LASTIP6"
+fi
 
 #get current ip
 IP=$(curl -s http://checkip4.spdyn.de/)
-echo "current IP is $IP"
+echo "current IPv4 is $IP"
+IP6=$(curl -s http://checkip6.spdyn.de/)
+#IP6=$(ip -6 addr show eth0 | grep inet6 | awk -F '[ \t]+|/' '{print $3}' | grep -v ^::1 | head -n 1)
+echo "current IPv6 is $IP6"
 
-
-
-#check ip and last check
-if [ "$IP" = "$LASTIP" ];then
-	if [ "$LASTCHECK" -le "$(( $(date +%s) - 10800 ))" ];then
-		echo "force ip update"
-	else
+#check if ipv4 and ipv6 are the same
+if [ "$IP" = "$LASTIP" ] && [ "$IP6" = "$LASTIP6" ]; then
 	echo "no ip change"
 	exit
-	fi
+else
+	echo "force ip update"
 fi
 
 #update string
-updateip(){
-RETURNCODE=$(curl -s --user $1:$2 "https://update.spdyn.de/nic/update?hostname=$1&myip=$IP&pass=$2")
+updateip() {
+	echo "Updating IPv4"
+	RETURNCODE=$(curl -s --user $1:$2 "https://update.spdyn.de/nic/update?hostname=$1&myip=$IP&pass=$2")
+	evalResult $RETURNCODE
+	echo "Updating IPv6"
+	RETURNCODE=$(curl -s --user $1:$2 "https://update.spdyn.de/nic/update?hostname=$1&myip=$IP6&pass=$2")
+	evalResult $RETURNCODE
+}
 
-
-# eval return code
-
-case $RETURNCODE in
+evalResult() {
+	# eval return code
+	case $1 in
 
 	nochg*)
-	echo "update done... IP was up2date"
-	;;
+		echo "update done... IP was up-to-date"
+		;;
 
 	good*)
-	echo "update done... IP changed to $IP"
-	;;
+		echo "update done... IP changed"
+		;;
 
 	abuse*)
-	>&2 echo "update failed: abuse error"
-	exit
-	;;
+		echo >&2 "update failed: abuse error"
+		exit
+		;;
 
 	badauth*)
-	>&2 echo "update failed: wrong user or password"
-	exit
-	;;
+		echo >&2 "update failed: wrong user or password"
+		exit
+		;;
 
 	numhost*)
-	>&2 echo "update failed: you try to update more as 20 hosts"
-	exit
-	;;
+		echo >&2 "update failed: you try to update more as 20 hosts"
+		exit
+		;;
 
 	notfqdn*)
-	>&2 echo "update failed: host is not a FQDN"
-	exit
-	;;
+		echo >&2 "update failed: host is not a FQDN"
+		exit
+		;;
 
 	!yours*)
-	>&2 echo "update failed: host is not assigned to your account"
-	exit
-	;;
+		echo >&2 "update failed: host is not assigned to your account"
+		exit
+		;;
 
 	fatal*)
-	>&2 echo "update failed: host is disabled"
-	exit
-	;;
+		echo >&2 "update failed: host is disabled"
+		exit
+		;;
 
 	nohost*)
-	>&2 echo "update failed: host not available or deleted"
-	exit
-	;;
+		echo >&2 "update failed: host not available or deleted"
+		exit
+		;;
 
-
-esac
+	esac
 
 }
 
-
-
 # load domains and passwords from spdnsupdater.conf
-if [ -f ~/.spdnsupdater.conf ];then
+if [ -f ~/.spdnsupdater.conf ]; then
 	echo "import config from ~/.spdnsupdater.conf "
-	source ~/.spdnsupdater.conf 
+	source ~/.spdnsupdater.conf
 else
 	echo "conf file \"$HOME/.spdnsupdater.conf\" not found"
 	exit
 fi
 
 # calc end for sequence
-e=$(( $(echo ${DOMAIN[*]} | wc -w) - 1 )) 
-
+e=$(($(echo ${DOMAIN[*]} | wc -w) - 1))
 
 #perform update
-for (( i=0; i<=$e; i++ ));do
+for ((i = 0; i <= $e; i++)); do
 	echo " "
 	echo "Update domain ${DOMAIN[$i]}"
 	updateip ${DOMAIN[$i]} ${PASSWORD[$i]}
 	echo " "
 done
-echo "LASTIP=$IP" > /tmp/lastip
-echo "LASTCHECK=$(date +%s)" >> /tmp/lastip
-
-
+echo "LASTIP=$IP" >/tmp/lastip
+echo "LASTIP6=$IP6" >/tmp/lastip6
 
 exit
